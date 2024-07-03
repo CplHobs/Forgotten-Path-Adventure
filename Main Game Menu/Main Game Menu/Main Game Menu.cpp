be40e7ca6668fc1edb7e368d3ef578a65f38ffb3
EliCodes24 <eli.pena907@gmail.com>
2024-07-03 10:56:47 -0800

    Update Main Game Menu.cpp
    adding my function and how we can direct the user to each path as their own function

--- Main Game Menu/Main Game Menu/Main Game Menu.cpp
+++ Main Game Menu/Main Game Menu/Main Game Menu.cpp
@@ -9,59 +9,42 @@ void testFunction();
 
 int main()
 {
-    int user_choice=0;
-    // Basic into text, currently a placeholder for now
-    cout << "This is where we will add the text intro for the user to explain our code/game" << endl;
+    int user_choice = 0;
+    // Basic intro text
+    cout << "Welcome to the Consortium Library" << endl;
+    cout << "Where would you like to go?" << endl;
     
     // The main loop of our game that contols what the input will do set to 20 for now 
     while (user_choice != 20) {
 
         cout << "MENU:" << endl
-            << "1: Path" << endl
-            << "2: Path" << endl
-            << "3: Path" << endl
-            << "4: Path" << endl
-            << "5: Path" << endl
-            << "6: Path" << endl
-            << "7: Path" << endl
-            << "8: Path" << endl
-            << "9: Path" << endl
-            << "20: Path" << endl;
+        << "1: Archives and Special Collections" << endl
+            << "2: Natural Science Building" << endl
+            << "3: Learning Commons" << endl;
         // For getting the user input to select what option in the game menu to select
         cin >> user_choice;
+
         // switch statment to control the user options currenly set to 9 cases with the option to add or remove as needed:
         switch (user_choice)
         {
         case 1: {
-            // code to be exicuted here:
+            // call function for path to Archives
         }
         case 2: {
-            // code to be exicuted here:
+            // call function for path to Science Building
         }
         case 3: {
-            // code to be exicuted here:
-        }
-        case 4: {
-            // code to be exicuted here:
-        }
-        case 5: {
-            // code to be exicuted here:
-        }
-        case 6: {
-            // code to be exicuted here:
-        }
-        case 7: {
-            // code to be exicuted here:
-        }
-        case 8: {
-            // code to be exicuted here:
-        }
-        case 9: {
-            // code to be exicuted here:
+            // call function for path to Learning Commons
+            void pathToLearningCommons();
         }
         default: {
             // set this to the exit code in case the user enters a random input that way it terminates the code:
             user_choice = 20;
+            cout << "You have entered a value outside of 1-3.\n"
+            << "Directing you to a random path.\n";
+            
+            // add code to pick a random path for the user or offer them a new selection
+
             break;
         }
         }
@@ -76,3 +59,39 @@ void testFunction() {
     // Place holder funtion to be removed
 }
 
+
+void pathToLearningCommons() {
+    cout << "You will need to go right moving beyond the front desk.\n"
+        << "1. Go beyond the front desk\n"
+        << "2. Investigate the vending machine\n"
+        << "3. Search the area for a cookie\n";
+    int userInput = 0, userTool = 0, numCookies = 0;
+    cin >> userInput;
+
+    while (userInput <= 1 || userInput >= 3) {
+        cout << "You have enter an invalid input. Pleasse select again\n";
+        cin >> userInput;
+    }
+    // take the user through their first set of options on their path
+    if (userInput == 1) {
+        cout << "You begin your journey to the learning commons.\n";
+        cout << "A small dog approaches. What would you like to do?\n";
+    }
+
+    if (userInput == 2) {
+        cout << "You walk up to the vending machine just beyond the entrance.\n"
+            << "Inside you see many possible tools for the journey.\n"
+            << "Select which item you would like to gain:\n"
+            << "1: Cardstock of Memory\n"
+            << "2: Pencil of Piercing\n"
+            << "3: Ballistic Eraser\n";
+    }
+
+    if (userInput == 3) {
+        cout << "You check around the area for a snack\n"
+            << "There looks to be something shiny in the bookshelf\n"
+            << "You gain one cookie\n";
+        numCookies++;
+    }
+    cout << "";
+}
