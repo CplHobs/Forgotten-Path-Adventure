44a76dea663b7ca0777ad4be5e3ae23b07dd91ba
CplHobs <eric.hobson.907@gmail.com>
2024-07-10 11:50:55 -0800

    Added my path code so far along with incorparting info from Eli's menu setup

--- Main Game Menu/Main Game Menu/Main Game Menu.cpp
+++ Main Game Menu/Main Game Menu/Main Game Menu.cpp
@@ -4,75 +4,111 @@
 
 using namespace std;
 
-// here is where we declare our functions: 
-void testFunction();
+// here is where we declare our functions for the paths: 
+void pathToArchives();
+void pathToLearningCommons();
+
+// Functions to be used in our paths
+int getUserIntInput(int min, int max);
 
 int main()
 {
-    int user_choice=0;
-    // Basic into text, currently a placeholder for now
-    cout << "This is where we will add the text intro for the user to explain our code/game" << endl;
-    
+    int user_choice = 0;
+    // Basic intro text
+    cout << "Welcome to the Consortium Library" << endl;
+    cout << "Where would you like to go?" << endl;
+
     // The main loop of our game that contols what the input will do set to 20 for now 
     while (user_choice != 20) {
 
         cout << "MENU:" << endl
-            << "1: Path" << endl
-            << "2: Path" << endl
-            << "3: Path" << endl
-            << "4: Path" << endl
-            << "5: Path" << endl
-            << "6: Path" << endl
-            << "7: Path" << endl
-            << "8: Path" << endl
-            << "9: Path" << endl
-            << "20: Path" << endl;
+            << "1: Archives and Special Collections" << endl
+            << "2: Natural Science Building" << endl
+            << "3: Learning Commons" << endl;
         // For getting the user input to select what option in the game menu to select
         cin >> user_choice;
+
         // switch statment to control the user options currenly set to 9 cases with the option to add or remove as needed:
         switch (user_choice)
         {
         case 1: {
-            // code to be exicuted here:
+            // call function for path to Archives
+            void pathToArchives();
         }
         case 2: {
-            // code to be exicuted here:
+            // call function for path to Science Building
         }
         case 3: {
-            // code to be exicuted here:
-        }
-        case 4: {
-            // code to be exicuted here:
-        }
-        case 5: {
-            // code to be exicuted here:
-        }
-        case 6: {
-            // code to be exicuted here:
-        }
-        case 7: {
-            // code to be exicuted here:
-        }
-        case 8: {
-            // code to be exicuted here:
-        }
-        case 9: {
-            // code to be exicuted here:
+            // call function for path to Learning Commons
+            void pathToLearningCommons();
         }
         default: {
             // set this to the exit code in case the user enters a random input that way it terminates the code:
             user_choice = 20;
+            cout << "You have entered a value outside of 1-3.\n"
+                << "Directing you to a random path.\n";
+
+            // add code to pick a random path for the user or offer them a new selection
+
             break;
         }
         }
     }
-
-
-    return 0;
 }
 
 // here is where we will define our functions 
-void testFunction() {
-    // Place holder funtion to be removed
+void pathToArchives() {
+    int user_choice = 0;
+
+    cout << "You Selected the Archives and Special Collections path." << endl
+        << "Menu:" << endl
+        << "1: Proceed to the right and past the main desk" << endl
+        << "2: " << endl
+        << "3: " << endl;
+    user_choice = getUserIntInput(1, 3);
+    switch (user_choice)
+    {
+    case 1: {
+        cout << "After moving past the main desk you see a large open area with the windows and students to your right: " << endl
+            << "Menu: " << endl
+            << "1: " << endl
+            << "2: " << endl
+            << "3: " << endl;
+        user_choice = getUserIntInput(1, 3);
+    }
+    case 2: {
+
+    }
+    case 3: {
+
+    }default: {
+        cout << "Error in switch value." << endl;
+    }
+    }
+
+
 }
 
+void pathToLearningCommons() {
+
+}
+
+int getUserIntInput(int min, int max) {
+    // takes in the min value from the menu options and the max value. 
+    // Insures user enters a valid value to select the option from the menu.
+    // Returns a valid user int value for the menu. 
+    int user_choice = 0;
+    while (true)
+    {
+        cin >> user_choice;
+        if (user_choice >= min && user_choice <= max) {
+            return user_choice;
+        }
+        else
+        {
+            cout << "You have entered a value outside of " << min << " and " << max << endl
+                << "Please try again." << endl;
+        }
+    }
+
+}
